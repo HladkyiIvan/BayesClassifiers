7db2bb3ce62cebaa022bb1f0aa8e01f4464a9508
Ivan Hladkyi <Ivan_Hladkyi@epam.com>
2020-06-15 21:55:53 +0300

    updated package

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='BayesClassifiers',
-      version='0.2',
+      version='0.3',
       description='NaiveBayes | BayesClassifier | SemiparametricBayesClassifier',
       url='https://github.com/HladkyiIvan/BayesClassifiers',
       author='Hladkyi Ivan',
